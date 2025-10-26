e9640260c5b3bd13a07859f44a3c8cc77b4bf83e
Alind <alindkumawat107github@gmail.com>
2025-10-26 19:33:01 +0530

    updates the header.jsx code to it's original for main branch.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,16 @@
 
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 
 const Header = () => {
-  const logo = "https://cdn-icons-png.flaticon.com/512/2921/2921222.png";
-
-  return (
-    <AppBar position="static" color="secondary">
-      <Toolbar style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-        <img 
-          src={logo} 
-          alt="Invoice Logo" 
-          style={{ width: 40, height: 40 }} 
-        />
-        <Typography 
-          variant="h6" 
-          component="div" 
-          sx={{ fontWeight: 'bold', letterSpacing: 1 }}
-        >
-          Invoice
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
-};
+    const logo = "https://dashboard.getinvoice.co/dboard/img/logo.png";
+    
+    return (
+        <AppBar position="static" color="secondary">
+            <Toolbar>
+                <img src={logo} alt="logo" style={{ width: 120 }} />
+            </Toolbar>
+        </AppBar>
+    )
+}
 
 export default Header;
