c70e37c1f3bd920cd3cc688f1f63c59885d535ea
Alind <alindkumawat107github@gmail.com>
2025-10-31 01:39:24 +0530

    till now extended the JPA interface to store Invoice(modal) -> (data) into MySQL

---
+++ server/src/main/java/com/invoiceprocessing/server/services/InvoiceServiceImpl.java
@@ -0,0 +1,10 @@
+package com.invoiceprocessing.server.services;
+
+import com.invoiceprocessing.server.model.Invoice;
+
+public class InvoiceServiceImpl implements InvoiceService{
+    @Override
+    public Invoice addInvoice(Invoice invoice){
+        return null;
+    }
+}
