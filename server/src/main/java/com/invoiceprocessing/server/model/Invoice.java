fbd36822b6c622d9ede33df8c24b98b9ec91766e
Alind <alindkumawat107github@gmail.com>
2025-10-31 02:36:49 +0530

    added @GeneratedValue and @id annotation for unique id generation and @Entity for making Database named 'Invoice'automatically in MySQL

--- server/src/main/java/com/invoiceprocessing/server/model/Invoice.java
+++ server/src/main/java/com/invoiceprocessing/server/model/Invoice.java
@@ -1,6 +1,14 @@
 package com.invoiceprocessing.server.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String vendor;
     private String product;
