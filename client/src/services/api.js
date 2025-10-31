b45b15ba3cf6a894c92ab3dd4e5d9ce18084f1ae
Alind <alindkumawat107github@gmail.com>
2025-10-31 18:32:24 +0530

    Configured API URL in api.js and added @CrossOrigin annotation in controller to allow frontend requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = "";
+const API_URL = "http://localhost:8080";
 
 export const saveInvoice = async (payload) => {
     try{
--- server/src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
+++ server/src/main/java/com/invoiceprocessing/server/controller/InvoiceController.java
@@ -3,11 +3,13 @@ package com.invoiceprocessing.server.controller;
 import com.invoiceprocessing.server.model.Invoice;
 import com.invoiceprocessing.server.services.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class InvoiceController {
 
     @Autowired
