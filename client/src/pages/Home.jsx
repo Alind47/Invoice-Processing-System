7aa0f3c2d558ad2d4a86ffb4463d407221083471
Alind <alindkumawat107github@gmail.com>
2025-11-03 14:49:07 +0530

    final: integrated invoice CRUD flow and synced client-server functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,32 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import { Box, Typography, Button } from "@mui/material";
 import AddInvoice from "../components/AddInvoice";
+import Invoices from "../components/Invoices";
+import { getAllInvoice, deleteInvoice } from "../services/api";
 
 const Home = () => {
     const [addInvoice, setAddInvoice] = useState(false);
+    const [invoices, setInvoices] = useState([]);
+
+    useEffect( () => {
+        const getData = async () => {
+            const response = await getAllInvoice();
+            setInvoices(response.data);
+        }
+        getData();
+    }, [addInvoice]);
 
     const toggleInvoice = () => {
         setAddInvoice(true);
+    }
+
+    const removeInvoice = async (id) => {
+        await deleteInvoice(id);
+
+        const updatedInvoice = invoices.filter(invoice => invoice.id === id);
+        setInvoices(updatedInvoice); 
     }
 
     return(
@@ -24,6 +42,12 @@ const Home = () => {
                       >Add Invoices</Button>
                 }       
                 { addInvoice && <AddInvoice setAddInvoice={setAddInvoice} /> }
+                <Box>
+                    <Invoices
+                       invoices={invoices}
+                       removeInvoice={removeInvoice} 
+                    />
+                </Box>
             </Box>
         </>
     )
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -11,3 +11,21 @@ export const saveInvoice = async (payload) => {
         return error.response.data;
     }
 }
+
+export const getAllInvoice = async () => {
+    try{
+        return await axios.get(`${API_URL}/invoice`);
+    }catch(error){
+        console.log('Error: ', error.message);
+        return error.response.data;
+    }
+}
+
+export const deleteInvoice = async (id) => {
+    try{
+        return await axios.delete(`${API_URL}/invoice/${id}`);
+    } catch(error){
+        console.log('Error: ', error.message);
+        return error.response.data;
+    }
+}
